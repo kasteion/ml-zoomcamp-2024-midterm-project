09af9bae37931d06181f241312a5d42a31b91585
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2024-11-25 05:03:51 -0500

    (training) model training

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# ml-zoomcamp-2024-midterm-project
-Machine Learning Zoomcamp 2024 Midterm Project
+# Fraud Detection in Energy Consumption
+
+In industries with high volumes of transactional data, detecting anomalies such as fraud is a critical task for maintaining operational integrity and reducing financial losses. The goal of this model is to identify fraudulent activity among clients based on historical data, including demographic information and usage patterns.
+
+The dataset comprises two key components:
+
+1. **Client Data**: This dataset contains general information about the clients, such as their unique IDs, categories, regional and district assignments, and the date when they were onboarded. This data also includes a binary target column that indicates whether a client has been flagged for fraud in the past.
+
+2. **Invoice Data**: This dataset records detailed transactional information for each client. It includes various consumption levels, tariff types, and other metrics related to utility usage, such as meter readings, consumption coefficients, and the duration of billing cycles.
+
+These datasets are complementary: client data provides high-level demographic information, while invoice data captures granular details of client behavior over time.
+
+The primary challenge is to predict and identify fraudulent clients based on patterns in their behavior and transaction history. Fraudulent behavior often manifests in specific patterns, such as:
+
+1. Abnormally high or low consumption levels.
+2. Frequent discrepancies in meter readings.
+3. Multiple tariff types or unusual invoice configurations for the same client.
+4. The location of the client could also be an important factor.
+
+However, distinguishing fraudulent clients from legitimate ones is non-trivial due to:
+
+1. **Data Complexity**: The invoice dataset contains multiple interdependent features that need to be aggregated and interpreted in the context of client-level data.
+2. **Data Imbalance**: Fraud cases typically represent a small fraction of the overall dataset, which poses challenges for model training and evaluation.
+
+This solution will be used by operational teams to:
+
+1. **Prioritize Investigations**: Identify high-risk clients who need further review or monitoring.
+2. **Improve Resource Allocation**: Focus efforts on clients most likely to be fraudulent, optimizing the use of investigative resources.
---
+++ train.py
@@ -0,0 +1,92 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.tree import DecisionTreeClassifier
+import pickle
+
+# Feature lists
+category_cols = ['client_catg', 'region', 'district']
+numeric_cols = ['age_months', 'consumption_level_1_mean',
+    'consumption_level_1_std', 'consumption_level_1_min',
+    'consumption_level_1_max', 'consumption_level_2_mean',
+    'consumption_level_2_std', 'consumption_level_2_min',
+    'consumption_level_2_max', 'consumption_level_3_mean',
+    'consumption_level_3_std', 'consumption_level_3_min',
+    'consumption_level_3_max', 'consumption_level_4_mean',
+    'consumption_level_4_std', 'consumption_level_4_min',
+    'consumption_level_4_max', 'total_consumption_mean',
+    'total_consumption_std', 'total_consumption_min',
+    'total_consumption_max']
+
+# Model parameters
+max_depth=20
+min_samples_leaf=5
+
+# Output file
+output_file = f'model_d={max_depth}s={min_samples_leaf}.bin'
+
+# Load data
+df_clients = pd.concat([pd.read_csv('data/client_train.csv')])
+df_invoices = pd.concat([pd.read_csv('data/invoice_train.csv')])
+
+# Editing clients dataset
+df_clients['district'] = df_clients['disrict']
+del df_clients['disrict']
+df_clients.creation_date = pd.to_datetime(df_clients.creation_date, dayfirst=True)
+df_clients['client_id'] = df_clients['client_id'].str.lower()
+df_clients['district'] = 'district_' + df_clients['district'].astype('str')
+df_clients['region'] = 'region_' + df_clients['region'].astype('str')
+df_clients['client_catg'] = 'client_catg_' + df_clients['client_catg'].astype('str')
+df_clients['target'] = df_clients['target'].astype(int)
+
+# Editing invoices dataset
+df_invoices['client_id'] = df_invoices['client_id'].str.lower()
+df_invoices['counter_type'] = df_invoices['counter_type'].str.lower()
+df_invoices['counter_status'] = df_invoices['counter_statue']
+df_invoices['consumption_level_1'] = df_invoices['consommation_level_1']
+df_invoices['consumption_level_2'] = df_invoices['consommation_level_2']
+df_invoices['consumption_level_3'] = df_invoices['consommation_level_3']
+df_invoices['consumption_level_4'] = df_invoices['consommation_level_4']
+del df_invoices['counter_statue']
+del df_invoices['consommation_level_1']
+del df_invoices['consommation_level_2']
+del df_invoices['consommation_level_3']
+del df_invoices['consommation_level_4']
+df_invoices.counter_status = pd.to_numeric(df_invoices.counter_status, errors='coerce').fillna(999999).astype(int)
+df_invoices = df_invoices[df_invoices.counter_status <= 5]
+df_invoices['counter_status'] = 'counter_status_' + df_invoices['counter_status'].astype(str)
+df_invoices.invoice_date = pd.to_datetime(df_invoices.invoice_date, yearfirst=True)
+
+# Extending dataset
+max_date = df_invoices.invoice_date.max()
+df_clients_extended = df_clients.copy()
+df_invoices_extended = df_invoices.copy()
+df_invoices_extended = df_invoices_extended.merge(df_clients[['client_id', 'target']], on='client_id', how='left')
+df_clients_extended['age_months'] = (max_date.year - df_clients_extended['creation_date'].dt.year) * 12 + (max_date.month-  df_clients_extended['creation_date'].dt.month)
+df_invoices_extended['total_consumption'] = (df_invoices_extended['consumption_level_1'] + df_invoices_extended['consumption_level_2'] + df_invoices_extended['consumption_level_3'] + df_invoices_extended['consumption_level_4'])
+consumption_cols = ['consumption_level_1', 'consumption_level_2', 'consumption_level_3', 'consumption_level_4', 'total_consumption']
+for c in consumption_cols:
+    df_agg = df_invoices_extended.groupby('client_id')[c].agg(['mean', 'std', 'min', 'max']).reset_index()
+    df_agg.rename(columns={'mean': f'{c}_mean', 'std': f'{c}_std', 'min': f'{c}_min', 'max': f'{c}_max'}, inplace=True)
+    df_clients_extended = df_clients_extended.merge(df_agg, on='client_id', how='left')
+df_clients_extended = df_clients_extended.fillna(0)
+
+# Spliting dataset
+df_clients_full_train, df_clients_test = train_test_split(df_clients_extended, test_size=0.2, random_state=42)
+df_clients_full_train.reset_index(drop=True)
+df_clients_test.reset_index(drop=True)
+y_clients_full_train = df_clients_full_train.target
+y_clients_test = df_clients_test.target
+del df_clients_full_train['target']
+del df_clients_test['target']
+
+# Training model
+dicts = df_clients_full_train[category_cols + numeric_cols].to_dict(orient='records')
+dv = DictVectorizer(sparse=False)
+X_train = dv.fit_transform(dicts)
+
+dt = DecisionTreeClassifier(max_depth=max_depth, min_samples_leaf=min_samples_leaf)
+dt.fit(X_train, y_clients_full_train)
+
+with open(output_file, 'wb') as file:
+    pickle.dump((dv, dt), file)
